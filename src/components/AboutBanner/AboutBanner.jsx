fe2c85c158a6c8889a726786b9ec2fbb5c6cd2d6
Ed <lookatemail@gmail.com>
2020-09-27 16:29:16 +0100

    Add custom fonts to about component

--- src/components/AboutBanner/AboutBanner.jsx
+++ src/components/AboutBanner/AboutBanner.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+
 import colors from '../../config/colors';
 
 export default function AboutBanner({ props }) {
   return (
-    <div style={styles.container}>
-      <div>DESIGNER. THINKER. HUSBAND & FATHER.</div>
-      <div>
+    <div className="teko" style={styles.container}>
+      <div style={styles.about}>
+        DESIGNER. THINKER. HUSBAND & FATHER.Almost before we knew it, we had
+        left the
+      </div>
+      <div className="font">
         Currently designing @Uber. Formerly Experience Design Director @GoPro
         and Interactive Director @Focus Lab. Follow my work, words and photos
         below.
@@ -23,5 +27,9 @@ const styles = {
     justifyItems: 'center',
     height: '100vh',
     backgroundColor: colors.yellow,
+  },
+  about: {
+    fontSize: 24,
+    fontWeight: 600,
   },
 };
