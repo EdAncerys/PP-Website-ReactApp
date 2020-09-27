92a138c0c0f06f94524fececb1ea252153b94bad
Ed <lookatemail@gmail.com>
2020-09-27 17:12:24 +0100

    Add social media icons to SosialBanner

--- src/components/AboutBanner/SocialBanner.jsx
+++ src/components/AboutBanner/SocialBanner.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { AiFillGithub } from 'react-icons/ai';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { AiFillMediumSquare } from 'react-icons/ai';
+
+import colors from '../../config/colors';
 
 export default function SocialBanner({ props }) {
   return (
     <div style={styles.container}>
-      <div>Social links</div>
-      <AiFillGithub />
+      <AiFillLinkedin size={36} />
+      <AiFillGithub size={36} />
+      <AiOutlineInstagram size={36} />
+      <AiFillMediumSquare size={36} />
     </div>
   );
 }
 
 const styles = {
-  container: {},
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, auto)',
+    columnGap: 10,
+    alignContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+    marginBottom: 50,
+  },
 };
