3ccd4c969b51dafd6150909b98248d3c328545cb
Ed <lookatemail@gmail.com>
2020-10-02 17:12:21 +0100

    Add on hover efect to image component

--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsPhone } from 'react-icons/bs';
 
 import colors from '../config/colors';
 
 export default function ImageContainer({ children }) {
-  return <div style={styles.container}>{children}</div>;
-}
+  const [hover, setHover] = useState(false);
 
-const styles = {
-  container: {
-    display: 'grid',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 250,
-    width: 250,
-    border: `1px solid ${colors.white}`,
-    borderRadius: '50% 50% 10% 50%',
-    boxShadow: '5px 2px',
-    overflow: 'hidden',
-  },
-};
+  return (
+    <div
+      style={{
+        display: 'grid',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 250,
+        width: 250,
+        border: `1px solid ${colors.white}`,
+        borderRadius: '50% 50% 10% 50%',
+        boxShadow: '5px 2px',
+        overflow: 'hidden',
+      }}
+      onMouseOver={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {children}
+    </div>
+  );
+}
