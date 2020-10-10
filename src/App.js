2d37a4295c5d4b467020f87bccc2efc92be1c2bd
Ed <lookatemail@gmail.com>
2020-10-10 13:53:08 +0100

    Able to pass data to sessions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/App.css';
 import ContentBanner from './components/ContentBanner.jsx';
 import AboutBanner from './components/AboutBanner/AboutBanner';
@@ -11,12 +11,36 @@ import colors from './config/colors';
 export const AppContext = React.createContext();
 
 export default function App({ props }) {
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(false);
+
+  const SESSION_STORAGE_KEY = 'EdAncerysPortfolioWebSite.App';
 
   const manageContextState = {
     page: page,
     setPage: setPage,
   };
+
+  useEffect(() => {
+    // if (userSession) console.log(userSession);
+    // setPage(sessionStorage.page);
+
+    // const data = sessionStorage.getItem(SESSION_STORAGE_KEY);
+    // const userSession = JSON.parse(data);
+    // // setPage(userSession.page);
+    // console.log('local storage', userSession);
+
+    sessionStorage.setItem(
+      SESSION_STORAGE_KEY,
+      JSON.stringify(manageContextState)
+    );
+  }, [manageContextState]);
+
+  // useEffect(() => {
+  //   const data = sessionStorage.getItem(SESSION_STORAGE_KEY);
+  //   const userSession = JSON.parse(data);
+  //   setPage(userSession.page);
+  //   console.log('local storage', userSession);
+  // }, [page]);
 
   return (
     <AppContext.Provider
