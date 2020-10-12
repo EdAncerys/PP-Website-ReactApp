408a22a62079aed9e08c4e947ac65dcad817cc60
Ed <lookatemail@gmail.com>
2020-10-12 20:58:02 +0100

    Add Family Guy characters to TickTackToe component

--- src/pages/TickTackToePage.jsx
+++ src/pages/TickTackToePage.jsx
@@ -1,10 +1,32 @@
 import React, { useState } from 'react';
+import Brian from '../img/TickTackToeImg/Brian.png';
+import Chris from '../img/TickTackToeImg/Chris.png';
+import Glenn from '../img/TickTackToeImg/Glenn.png';
+import Herbert from '../img/TickTackToeImg/Herbert.png';
+import Joe from '../img/TickTackToeImg/Joe.png';
+import Lois from '../img/TickTackToeImg/Lois.png';
+import Meg from '../img/TickTackToeImg/Meg.png';
+import Peter from '../img/TickTackToeImg/Peter.png';
+import Stewie from '../img/TickTackToeImg/Stewie.png';
 import FamilyGuyCharacters from '../components/TickTackToeBanner/FamilyGuyCharacters';
 
 export const TickTackToeContext = React.createContext();
 
 export default function TickTackToePage({ props }) {
-  const [characters, setCharacters] = useState(['']);
+  const [playerOne, setPlayerOne] = useState();
+  const [playerTwo, setPlayerTwo] = useState();
+
+  const characters = [
+    Brian,
+    Chris,
+    Glenn,
+    Herbert,
+    Joe,
+    Lois,
+    Meg,
+    Peter,
+    Stewie,
+  ];
 
   const manageTickTackToeContext = { test: 'TickTackToe test context' };
 
