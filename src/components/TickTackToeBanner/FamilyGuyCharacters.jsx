5529caa921ce750119e55e76a5b7c74b7fc8ede7
Ed <lookatemail@gmail.com>
2020-10-13 07:16:53 +0100

    Refactor img component

--- src/components/TickTackToeBanner/FamilyGuyCharacters.jsx
+++ src/components/TickTackToeBanner/FamilyGuyCharacters.jsx
@@ -6,23 +6,29 @@ export default function FamilyGuyCharacters({ props }) {
 
   return (
     <div style={styles.container}>
-      {manageTickTackToeContext.characters.map((img) => (
-        <div key={img.id} style={styles.wrapper}>
-          <img
-            style={styles.img}
-            key={img.id}
-            onClick={(e) => console.log(img.id)}
-            alt={img.name}
-            src={img.name}
-          />
-        </div>
-      ))}
+      <div style={styles.wrapper}>
+        {manageTickTackToeContext.characters.map((img) => (
+          <div key={img.id} style={styles.imgContainer}>
+            <img
+              style={styles.img}
+              key={img.id}
+              onClick={(e) => console.log(img.id)}
+              alt={img.name}
+              src={img.name}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 const styles = {
   container: {
+    display: 'grid',
+    justifyContent: 'center',
+  },
+  wrapper: {
     display: 'grid',
     gridTemplateColumns: 'repeat(4, auto)',
     gridGap: 10,
@@ -31,7 +37,7 @@ const styles = {
     height: '60vh',
     overflow: 'auto',
   },
-  wrapper: {
+  imgContainer: {
     backgroundColor: 'tomato',
     alignSelf: 'center',
   },
