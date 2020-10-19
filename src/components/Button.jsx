64d194db52737fffe1b0b3b1b51860448fc4b491
Ed <lookatemail@gmail.com>
2020-10-19 15:20:12 +0100

    Refactor Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,47 +2,40 @@ import React, { useState, useContext } from 'react';
 import { AppContext } from '../App';
 
 import pagePalette from '../config/PagePalette';
-import colors from '../config/colors';
 
-export default function Button({
-  title = 'Title',
-  solid = true,
-  onClick,
-}) {
-  const [hover, setHover] = useState(false);
-  const { manageAppContext } = useContext(AppContext);
+export default function Button({ title = 'Title', solid = true, onClick }) {
+	const [ hover, setHover ] = useState(false);
+	const { manageAppContext } = useContext(AppContext);
 
-  const btnTextColor = solid
-    ? pagePalette[manageAppContext.page].secondary
-    : pagePalette[manageAppContext.page].primary;
-  const btnBackgroundColor = solid
-    ? pagePalette[manageAppContext.page].primary
-    : pagePalette[manageAppContext.page].secondary;
-  const hoverColor = pagePalette[manageAppContext.page].secondary;
+	const btnTextColor = solid
+		? pagePalette[manageAppContext.page].secondary
+		: pagePalette[manageAppContext.page].primary;
+	const btnBackgroundColor = solid
+		? pagePalette[manageAppContext.page].primary
+		: pagePalette[manageAppContext.page].secondary;
 
-  return (
-    <div
-      style={{
-        display: 'grid',
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: hover ? colors.white : btnTextColor,
-        backgroundColor: hover ? hoverColor : btnBackgroundColor,
-        border: `1px solid ${btnBackgroundColor}`,
-        borderRadius: 20,
-        fontWeight: 'bold',
-        fontSize: 'inherit',
-        padding: 5,
-        paddingLeft: 20,
-        paddingRight: 20,
-        margin: 10,
-        cursor: 'pointer',
-      }}
-      onClick={onClick}
-      onMouseOver={() => setHover(!hover)}
-      onMouseLeave={() => setHover(!hover)}
-    >
-      {title}
-    </div>
-  );
+	return (
+		<div
+			style={{
+				display: 'grid',
+				justifyContent: 'center',
+				alignItems: 'center',
+				color: hover ? btnBackgroundColor : btnTextColor,
+				backgroundColor: hover ? btnTextColor : btnBackgroundColor,
+				borderRadius: 20,
+				fontWeight: 500,
+				fontSize: 24,
+				padding: 5,
+				paddingLeft: 20,
+				paddingRight: 20,
+				margin: 10,
+				cursor: 'pointer'
+			}}
+			onClick={onClick}
+			onMouseOver={() => setHover(!hover)}
+			onMouseLeave={() => setHover(!hover)}
+		>
+			{title}
+		</div>
+	);
 }
