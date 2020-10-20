6f436976a731381a18776c52ab588f53659935bb
Ed <lookatemail@gmail.com>
2020-10-20 22:06:36 +0100

    Add board cell component

---
+++ src/components/TickTackToeBanner/BoardCell.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function BoardCell({ props }) {
+  return (
+    <div style={styles.container}>
+      <div>hello</div>
+    </div>
+  );
+}
+
+const styles = {
+  container: {}
+};
