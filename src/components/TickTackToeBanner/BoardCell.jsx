dac4924ac463209a24e38d4cf0e5255db7b7d2ca
Ed <lookatemail@gmail.com>
2020-10-21 22:19:30 +0100

    Push changes to git

--- src/components/TickTackToeBanner/BoardCell.jsx
+++ src/components/TickTackToeBanner/BoardCell.jsx
@@ -3,17 +3,20 @@ import { AppContext } from '../../App';
 
 export default function BoardCell({ id }) {
   const { manageAppContext } = useContext(AppContext);
+  const player = manageAppContext.playOneMove;
 
   const handleClick = (data) => {
-    const player = manageAppContext.playOneMove;
     const id = data.target.id;
 
-    if (player)
+    if (player) {
       manageAppContext.setPlayerOne([...manageAppContext.playerOne, id]);
-    if (!player)
+      manageAppContext.setPlayerOneMove(!player);
+    }
+    if (!player) {
       manageAppContext.setPlayerTwo([...manageAppContext.playerTwo, id]);
-    manageAppContext.setPlayerOneMove(!player);
-    console.log(id);
+      manageAppContext.setPlayerOneMove(!player);
+    }
+    console.log(manageAppContext.playOneMove);
   };
 
   return (
