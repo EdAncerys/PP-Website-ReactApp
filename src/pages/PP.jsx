19470322da614d6d1f35d3ed1d1e1c9633ea698f
Ed <lookatemail@gmail.com>
2020-10-28 23:13:51 +0000

    Add PP page

---
+++ src/pages/PP.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function PP({ props }) {
+  return (
+    <div style={styles.container}>
+      <div>PP Page</div>
+    </div>
+  );
+}
+
+const styles = {
+  container: {},
+};
