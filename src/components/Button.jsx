4930a97c4b3e2ef0e674e0a59e193be3fc1240c6
Ed <lookatemail@gmail.com>
2020-11-09 21:54:29 +0000

    Refactor Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,38 +4,39 @@ import { AppContext } from '../App';
 import pagePalette from '../config/PagePalette';
 
 export default function Button({ title = 'Title', solid = true, onClick }) {
-	const [ hover, setHover ] = useState(false);
-	const { manageAppContext } = useContext(AppContext);
+  const [hover, setHover] = useState(false);
+  const { manageAppContext } = useContext(AppContext);
 
-	const btnTextColor = solid
-		? pagePalette[manageAppContext.page].secondary
-		: pagePalette[manageAppContext.page].primary;
-	const btnBackgroundColor = solid
-		? pagePalette[manageAppContext.page].primary
-		: pagePalette[manageAppContext.page].secondary;
+  const btnTextColor = solid
+    ? pagePalette[manageAppContext.page].secondary
+    : pagePalette[manageAppContext.page].primary;
+  const btnBackgroundColor = solid
+    ? pagePalette[manageAppContext.page].primary
+    : pagePalette[manageAppContext.page].secondary;
 
-	return (
-		<div
-			style={{
-				display: 'grid',
-				justifyContent: 'center',
-				alignItems: 'center',
-				color: hover ? btnBackgroundColor : btnTextColor,
-				backgroundColor: hover ? btnTextColor : btnBackgroundColor,
-				borderRadius: 20,
-				fontWeight: 500,
-				fontSize: 24,
-				padding: 5,
-				paddingLeft: 20,
-				paddingRight: 20,
-				margin: 10,
-				cursor: 'pointer'
-			}}
-			onClick={onClick}
-			onMouseOver={() => setHover(!hover)}
-			onMouseLeave={() => setHover(!hover)}
-		>
-			{title}
-		</div>
-	);
+  return (
+    <div
+      style={{
+        display: 'grid',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        color: hover ? btnBackgroundColor : btnTextColor,
+        backgroundColor: hover ? btnTextColor : btnBackgroundColor,
+        borderRadius: 20,
+        fontWeight: 500,
+        fontSize: 24,
+        padding: 5,
+        paddingLeft: 20,
+        paddingRight: 20,
+        margin: 10,
+        cursor: 'pointer',
+      }}
+      onClick={onClick}
+      onMouseOver={() => setHover(!hover)}
+      onMouseLeave={() => setHover(!hover)}
+    >
+      {title}
+    </div>
+  );
 }
