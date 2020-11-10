da1cbb424a272edac2c35877bc124c70f06ff1f8
Ed <lookatemail@gmail.com>
2020-11-10 20:36:41 +0000

    Refactor HeaderBaner component and Introduction banner

--- src/components/HeaderForBanner.jsx
+++ src/components/HeaderForBanner.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { GiDwarfFace } from 'react-icons/gi';
 
-export default function HeaderForBanner({ color, pageNumber }) {
-	return (
-		<div
-			style={{
-				display: 'grid',
-				gridTemplateColumns: '10px 20px 10px 25px 10px',
-				alignItems: 'center',
-				fontSize: 18,
-				color: color
-			}}
-		>
-			<div>&#123;</div>
-			<GiDwarfFace />:<div>{pageNumber}</div>
-			<div>&#125;</div>
-		</div>
-	);
+export default function HeaderForBanner({ color, page }) {
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '10px 20px 10px 25px 10px',
+        alignItems: 'center',
+        fontSize: 18,
+        color: color,
+      }}
+    >
+      <div>&#123;</div>
+      <GiDwarfFace />:<div>{`00${page}`}</div>
+      <div>&#125;</div>
+    </div>
+  );
 }
--- src/components/IntroductionBanner/IntroductionBanner.jsx
+++ src/components/IntroductionBanner/IntroductionBanner.jsx
@@ -9,44 +9,51 @@ import PagePalette from '../../config/PagePalette';
 import HeaderForBanner from '../HeaderForBanner';
 
 export default function TickTackToeBanner({ props }) {
-	const { manageAppContext } = useContext(AppContext);
-	const mediaQuery = manageAppContext.mediaQuery;
+  const { manageAppContext } = useContext(AppContext);
+  const mediaQuery = manageAppContext.mediaQuery;
 
-	return (
-		<div className="bangers" style={styles.container}>
-			<div
-				style={{
-					display: 'grid',
-					gridTemplateRows: '50px auto 100px',
-					height: 550,
-					width: mediaQuery ? '95vw' : 600
-				}}
-			>
-				<HeaderForBanner color={PagePalette[1].secondary} pageNumber="001" />
-				<div style={styles.imageContainer}>
-					<ImageContainer
-						img={LegoManImg}
-						altText="Tick-Tack-Toe"
-						text={textDescription.AboutInfoBanner}
-						color={PagePalette[1].secondary}
-					/>
-				</div>
-				<FooterForBanner name="About" type="Informational" color={PagePalette[1].secondary} page={1} />
-			</div>
-		</div>
-	);
+  const pageNumber = 1;
+
+  return (
+    <div className="bangers" style={styles.container}>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateRows: '50px auto 100px',
+          height: 550,
+          width: mediaQuery ? '95vw' : 600,
+        }}
+      >
+        <HeaderForBanner color={PagePalette[1].secondary} page={pageNumber} />
+        <div style={styles.imageContainer}>
+          <ImageContainer
+            img={LegoManImg}
+            altText="Tick-Tack-Toe"
+            text={textDescription.AboutInfoBanner}
+            color={PagePalette[1].secondary}
+          />
+        </div>
+        <FooterForBanner
+          name="About"
+          type="Informational"
+          color={PagePalette[1].secondary}
+          page={pageNumber}
+        />
+      </div>
+    </div>
+  );
 }
 
 const styles = {
-	container: {
-		display: 'grid',
-		justifyContent: 'center',
-		backgroundColor: PagePalette[1].primary,
-		width: '100%'
-	},
-	imageContainer: {
-		display: 'grid',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
+  container: {
+    display: 'grid',
+    justifyContent: 'center',
+    backgroundColor: PagePalette[1].primary,
+    width: '100%',
+  },
+  imageContainer: {
+    display: 'grid',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 };
