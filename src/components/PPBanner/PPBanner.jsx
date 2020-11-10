38294d7381cc357f3f95ca4668874b9b7686e53b
Ed <lookatemail@gmail.com>
2020-11-10 11:22:00 +0000

    Refactor HeaderForBanner component - abple to pas content dinamicaly

--- src/components/PPBanner/PPBanner.jsx
+++ src/components/PPBanner/PPBanner.jsx
@@ -9,49 +9,49 @@ import HeaderForBanner from '../HeaderForBanner';
 import FooterForBanner from '../FooterForBanner';
 
 export default function TickTackToeBanner({ props }) {
-  const { manageAppContext } = useContext(AppContext);
-  const mediaQuery = manageAppContext.mediaQuery;
+	const { manageAppContext } = useContext(AppContext);
+	const mediaQuery = manageAppContext.mediaQuery;
 
-  return (
-    <div className="bangers" style={styles.container}>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateRows: '50px auto 100px',
-          height: 550,
-          width: mediaQuery ? '95vw' : 600,
-        }}
-      >
-        <HeaderForBanner color={PagePalette[2].secondary} />
-        <div style={styles.imageContainer}>
-          <ImageContainer
-            img={Coding}
-            altText="Tick-Tack-Toe"
-            text={textDescription.AboutInfoBanner}
-            color={PagePalette[2].secondary}
-          />
-        </div>
-        <FooterForBanner
-          name="See What I Can Do"
-          type="Informational"
-          color={PagePalette[2].secondary}
-          page={2}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="bangers" style={styles.container}>
+			<div
+				style={{
+					display: 'grid',
+					gridTemplateRows: '50px auto 100px',
+					height: 550,
+					width: mediaQuery ? '95vw' : 600
+				}}
+			>
+				<HeaderForBanner color={PagePalette[2].secondary} pageNumber="002" />
+				<div style={styles.imageContainer}>
+					<ImageContainer
+						img={Coding}
+						altText="Tick-Tack-Toe"
+						text={textDescription.AboutInfoBanner}
+						color={PagePalette[2].secondary}
+					/>
+				</div>
+				<FooterForBanner
+					name="See What I Can Do"
+					type="Informational"
+					color={PagePalette[2].secondary}
+					page={2}
+				/>
+			</div>
+		</div>
+	);
 }
 
 const styles = {
-  container: {
-    display: 'grid',
-    justifyContent: 'center',
-    backgroundColor: PagePalette[2].primary,
-    width: '100%',
-  },
-  imageContainer: {
-    display: 'grid',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		display: 'grid',
+		justifyContent: 'center',
+		backgroundColor: PagePalette[2].primary,
+		width: '100%'
+	},
+	imageContainer: {
+		display: 'grid',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 };
--- src/components/TickTackToeBanner/TickTackToeBanner.jsx
+++ src/components/TickTackToeBanner/TickTackToeBanner.jsx
@@ -9,49 +9,49 @@ import FooterForBanner from '../FooterForBanner';
 import PagePalette from '../../config/PagePalette';
 
 export default function TickTackToeBanner({ props }) {
-  const { manageAppContext } = useContext(AppContext);
-  const mediaQuery = manageAppContext.mediaQuery;
+	const { manageAppContext } = useContext(AppContext);
+	const mediaQuery = manageAppContext.mediaQuery;
 
-  return (
-    <div className="bangers" style={styles.container}>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateRows: '50px auto 100px',
-          height: 550,
-          width: mediaQuery ? '95vw' : 600,
-        }}
-      >
-        <HeaderForBanner color={PagePalette[3].secondary} />
-        <div style={styles.imageContainer}>
-          <ImageContainer
-            img={TickTackToeImg}
-            altText="Tick-Tack-Toe"
-            text={textDescription.TickTackToe}
-            color={PagePalette[3].secondary}
-          />
-        </div>
-        <FooterForBanner
-          name="Game"
-          type="Board Game: Tic Tack Toe"
-          color={PagePalette[3].secondary}
-          page={3}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="bangers" style={styles.container}>
+			<div
+				style={{
+					display: 'grid',
+					gridTemplateRows: '50px auto 100px',
+					height: 550,
+					width: mediaQuery ? '95vw' : 600
+				}}
+			>
+				<HeaderForBanner color={PagePalette[3].secondary} pageNumber="003" />
+				<div style={styles.imageContainer}>
+					<ImageContainer
+						img={TickTackToeImg}
+						altText="Tick-Tack-Toe"
+						text={textDescription.TickTackToe}
+						color={PagePalette[3].secondary}
+					/>
+				</div>
+				<FooterForBanner
+					name="Game"
+					type="Board Game: Tic Tack Toe"
+					color={PagePalette[3].secondary}
+					page={3}
+				/>
+			</div>
+		</div>
+	);
 }
 
 const styles = {
-  container: {
-    display: 'grid',
-    justifyContent: 'center',
-    backgroundColor: PagePalette[3].primary,
-    width: '100%',
-  },
-  imageContainer: {
-    display: 'grid',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		display: 'grid',
+		justifyContent: 'center',
+		backgroundColor: PagePalette[3].primary,
+		width: '100%'
+	},
+	imageContainer: {
+		display: 'grid',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 };
