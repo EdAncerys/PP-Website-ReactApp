2f991cd235e4621dcdb81ab9b1ad6cd7e95ea5df
Ed <lookatemail@gmail.com>
2020-11-14 14:59:36 +0000

    Add RPSBanner component

---
+++ src/components/RPS/RPSBanner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function RPSBanner({ props }) {
+  return (
+    <div style={styles.container}>
+      <div>RPS</div>
+    </div>
+  );
+}
+
+const styles = {
+  container: {},
+};
