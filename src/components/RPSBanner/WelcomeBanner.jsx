448f985812a71491e574b6a90a913f3fa5fa48bd
Ed <lookatemail@gmail.com>
2020-11-15 11:26:16 +0000

    Add player icons to the RPS page

--- src/components/RPSBanner/WelcomeBanner.jsx
+++ src/components/RPSBanner/WelcomeBanner.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../../App';
 
+import { GiGingerbreadMan } from 'react-icons/gi';
+import { FaRobot } from 'react-icons/fa';
+
 import PagePalette from '../../config/PagePalette';
 
 export default function WelcomeBanner({ props }) {
@@ -9,12 +12,17 @@ export default function WelcomeBanner({ props }) {
   return (
     <div style={{ color: PagePalette[manageAppContext.page].secondary }}>
       <div style={styles.title}>Hi and Welcome To The RPS Game</div>
+      <div style={styles.content}>
+        <GiGingerbreadMan size="10vh" />
+        <div>vs</div>
+        <FaRobot size="10vh" />
+      </div>
     </div>
   );
 }
 
 const styles = {
   title: {
-    fontSize: 30,
+    fontSize: 38,
   },
 };
