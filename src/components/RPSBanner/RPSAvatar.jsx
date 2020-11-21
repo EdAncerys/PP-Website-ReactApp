6d0b27d1be72f2ce4ee82ecb9a73a0fe38152c7c
Ed <lookatemail@gmail.com>
2020-11-21 15:28:00 +0000

    Able to setTimeOut for avatar component

--- src/components/RPSBanner/RPSAvatar.jsx
+++ src/components/RPSBanner/RPSAvatar.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
+import colors from '../../config/colors';
 
 export default function RPSAvatar({ name, avatar }) {
   const [click, setClick] = useState(false);
 
+  const color = click ? colors.red : '';
+
   const handleClick = () => {
-    console.log('click');
+    setClick(!click);
+    setTimeout(() => {
+      setClick(false);
+    }, 500);
   };
 
   return (
-    <div style={styles.container} onClick={() => handleClick()}>
+    <div
+      style={{ ...styles.container, ...{ color: color } }}
+      onClick={() => handleClick()}
+    >
       <div style={styles.name}>{name}</div>
       <div>{avatar}</div>
     </div>
