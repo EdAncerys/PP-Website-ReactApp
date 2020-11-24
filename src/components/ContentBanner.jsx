50396b082f01506e83e6fa4a8e68dacb18d7d1ba
Ed <lookatemail@gmail.com>
2020-11-24 22:27:23 +0000

    Push update to master - update webPage with latest changes

--- src/components/ContentBanner.jsx
+++ src/components/ContentBanner.jsx
@@ -11,7 +11,7 @@ export default function ContentBanner({ props }) {
       <IntroductionBanner />
       <PPBanner />
       <TickTackToeBanner />
-      <RPSBanner />
+      {/* <RPSBanner /> */}
       <Footer />
     </div>
   );
